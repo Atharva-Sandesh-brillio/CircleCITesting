941a652ee51fdf2fbe5172edade1507cb73a47fe
Atharva-Sandesh-brillio <Atharva.Sandesh@brillio.com>
2024-07-14 20:19:38 +0530

    Updating

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world', () => {
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
+  const linkElement = await screen.findByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
